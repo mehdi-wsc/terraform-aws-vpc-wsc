2befa0de9ec574cf58c9f244a05aa7ee87d8cb9f
mehdi-wsc <62596892+mehdi-wsc@users.noreply.github.com>
2020-03-24 17:04:16 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "aws_eip" "nat_instance" {
 
 resource "aws_nat_gateway" "nat_instance" {
   allocation_id = aws_eip.nat_instance.id
-  subnet_id=aws_subnet.public.1.id
+  subnet_id=aws_subnet.public.0.id
 }
 
 #Internet Getway
