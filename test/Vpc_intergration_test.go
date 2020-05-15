9911e0a3d5c3c0e0fd902240a37c63ae56123b42
mehdi <mehdi.kerbedj@wescale.fr>
2020-05-15 13:07:17 +0200

    upgrade test

---
+++ test/Vpc_intergration_test.go
@@ -0,0 +1,32 @@
+//build test
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestIntegrationVpc(t *testing.T) {
+	t.Parallel()
+	region := "eu-west-1"
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../example",
+		VarFiles:     []string{"variables.tfvars"},
+	}
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.RunTerraformCommandAndGetStdoutE(t, terraformOptions, terraform.FormatArgs(terraformOptions, "fmt ")...)
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	output_public := terraform.Output(t, terraformOptions, "public_ids")
+
+	aws.IsPublicSubnet(t, output_public, region)
+
+	output_private := terraform.Output(t, terraformOptions, "private_ids")
+
+	aws.IsPublicSubnet(t, output_private, region)
+}
---
+++ test/Vpc_unit_test.go
@@ -0,0 +1,22 @@
+//bui
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUnitVpc(t *testing.T) {
+	t.Parallel()
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../example",
+		VarFiles:     []string{"variables.tfvars"},
+	}
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	output := terraform.InitAndPlan(t, terraformOptions)
+	assert.Contains(t, output, "Plan:")
+}
