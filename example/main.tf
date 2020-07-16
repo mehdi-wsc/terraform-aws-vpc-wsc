8d9c4a6bd6ab268cf30feab857188d912b9fa900
mehdi <mehdi.kerbedj@wescale.fr>
2020-07-16 21:54:05 +0200

    update

--- example/main.tf
+++ example/main.tf
@@ -1,16 +1,15 @@
 provider "aws" {
   region = var.region
 }
-
 module "vpc-wsc" {
-  source               = "../"
-  # version              = "0.0.3"
+  source               = "mehdi-wsc/vpc-wsc/aws"
+  version              = "0.0.3"
   group                = var.group
   env                  = var.env
   region               = var.region
   vpc_cidr             = var.vpc_cidr
   public_subnet_count  = var.private_subnet_count
-	  private_subnet_count = var.private_subnet_count
+  private_subnet_count = var.private_subnet_count
   cidr_block_private   = var.cidr_block_private
   cidr_block_public    = var.cidr_block_public
 }
