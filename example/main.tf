c8a493d8ccecb3f0226fb22d32f05b1285f5b797
mehdi-wsc <62596892+mehdi-wsc@users.noreply.github.com>
2020-07-16 21:58:57 +0200

    Update main.tf

--- example/main.tf
+++ example/main.tf
@@ -9,7 +9,7 @@ module "vpc-wsc" {
   env                  = var.env
   region               = var.region
   vpc_cidr             = var.vpc_cidr
-  public_subnet_count  = var.private_subnet_coun
+  public_subnet_count  = var.private_subnet_count
   private_subnet_count = var.private_subnet_count
   cidr_block_private   = var.cidr_block_private
   cidr_block_public    = var.cidr_block_public
