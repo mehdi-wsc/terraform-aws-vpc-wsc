ed9438e8d270430b006fadc3334afdcd86ed09ae
mehdi <mehdi.kerbedj@wescale.fr>
2020-07-17 16:06:46 +0200

    update

--- test/syntax_test.go
+++ test/syntax_test.go
@@ -6,7 +6,14 @@ import (
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
-func TestSyntax(t *testing.T) {
+func TestSyntaxForRoot(t *testing.T) {
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../",
+	}
+	terraform.RunTerraformCommand(t, terraformOptions, terraform.FormatArgs(terraformOptions, "fmt", "--check")...)
+
+}
+func TestSyntaxForExample(t *testing.T) {
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../example",
 	}
